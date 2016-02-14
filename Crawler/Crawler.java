7ac28fe7d6cbb95b016f3318aef04a868c870d38
Bill <iamkg05@gmail.com>
2016-02-13 23:02:09 -0600

    finished date tracking and logging

--- Crawler/Crawler.java
+++ Crawler/Crawler.java
@@ -4,8 +4,12 @@ public class Crawler
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		Date_Tracker dateTraking = new Date_Tracker();
+		String date = "2016-11-31";
 
+		for(int i = 0; i < 365; i++){
+			date = dateTraking.get_next_date(date);
+		}
 	}
 
 }
--- Crawler/Date_Tracker.java
+++ Crawler/Date_Tracker.java
@@ -1,10 +1,38 @@
+import java.util.*;
+import java.text.*;
+import java.util.logging.*;
+import java.io.*;
 
 public class Date_Tracker {
 
-	String get_next_date()
+	String get_next_date(String dateString)
 	{
-		//TODO
-		//get the next date
-		return null;
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar currentDate = null;
+		Date startDate = null;
+		Date date = null;
+
+		try{
+			startDate = formatter.parse(dateString);
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+	   	currentDate = Calendar.getInstance();
+		currentDate.setTime(startDate);
+		currentDate.add(Calendar.DATE, 1);
+		date = currentDate.getTime();		
+		dateString = formatter.format(date).toString();
+		log(dateString);
+		return dateString;
+	}
+
+	private static void log(String dateString)
+	{
+		try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("log.txt", true)))) {
+		    out.println(dateString);
+		}catch (IOException e) {
+		    e.printStackTrace();
+		}
 	}
 }
