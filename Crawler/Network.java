aec61bc91467e4f2f70d0122a63661c257d7b2fe
Divyesh T <1993sonu01@gmail.com>
2016-02-15 14:55:03 -0600

    Done the network part. good to go for testing

--- Crawler/Network.java
+++ Crawler/Network.java
@@ -1,11 +1,33 @@
-
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
 
 public class Network
 {
-    static String fetch(String URL)
+	private final String USER_AGENT = "Mozilla/5.0";
+    static String fetch(String url) throws Exception
     {
-        //TODO
+    	URL obj = new URL(url);
+    	HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+    	con.setRequestMethod("GET");
+    	con.setRequestProperty("User-Agent",USER_AGENT);
+    	int responseCode = con.getResponseCode();
+    	//TODO
+    	//handle 302 or 404 response codes
+    	BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+  		String input;
+  		StringBuffer response = new StringBuffer();
+  		while(input=in.readLine())!=null)
+		{
+			response.append(input);
+		}
+		in.close();
+
+
         //fetch webpage here and return string
-        return null;
+        return response.toString();
     }
 }
